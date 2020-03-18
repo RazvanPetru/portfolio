834298fe9eff52d2268bf1ce74c20e4257562c6e
Razvp <razvanpetru@users.noreply.github.com>
2020-03-18 14:23:29 +0200

    adding a nav-bar

---
+++ public/js/nav.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.lines');
+const navLinks = document.querySelector('.nav__links');
+const links = document.querySelector('nav__links li');
+const items = document.getElementsByClassName('nav__links-item');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+
+    navLinks.classList.toggle('open');
+});
+
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', () => {
+        navLinks.classList.remove('open');
+        hamburger.classList.remove('is-active');
+    });
+}
