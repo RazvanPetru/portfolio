11e97f42ab5483d4e3fe53a3f4af948e7264f6b5
Razvp <razvanpetru@users.noreply.github.com>
2020-03-19 14:59:36 +0200

    adding 404 page

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(
+    bodyparser.urlencoded({
+        extended: false
+    })
+);
 // static files
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // routes
-const resume = require('./routes/resume');
+const resumeRoutes = require('./routes/resume');
 
-
+app.use(resumeRoutes);
 
 // first page && 404 page
 app.get('/', (req, res) => {
@@ -19,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).send('<h1>404</h1>');
+    res.status(404).sendFile(__dirname + '/views/404.html');
 });
 
 // port
--- routes/resume.js
+++ routes/resume.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const router = express.Router();
 
-
+router.get('/resume', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/resume.pdf'));
+})
 
 module.exports = router;
