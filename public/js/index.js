da02e81e3ebbfafd5dc86f5336c309e758fdd6b9
Razvp <razvanpetru@users.noreply.github.com>
2020-03-27 16:03:49 +0200

    adding read more to projects

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,39 @@ const hamburger = document.querySelector('.lines');
 const navLinks = document.querySelector('.nav__links');
 const links = document.querySelector('nav__links li');
 const items = document.getElementsByClassName('nav__links-item');
+const desc = document.getElementsByClassName('container__description');
+const btn = document.getElementsByClassName('container__btn');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
-
     navLinks.classList.toggle('open');
 });
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+        desc[i].classList.toggle('hide');
+        let bt = btn[i];
+
+        if (bt.innerHTML === 'Read More') {
+            bt.innerHTML = 'Read Less';
+        } else {
+            bt.innerHTML = 'Read More';
+        }
+
+        TweenMax.from('.container__description', .5, {
+            delay: .1,
+            opacity: 0,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.from('.container__btn', .3, {
+            delay: .1,
+            opacity: 0,
+            y: -10,
+            ease: Expo.easeInOut
+        })
+    })
+}
 
 for (let i = 0; i < items.length; i++) {
     items[i].addEventListener('click', () => {
@@ -23,7 +50,6 @@ function scrollSmoothTo(elementId) {
         behavior: 'smooth'
     });
 }
-
 // GSAP EFFECTS
 
 TweenMax.to('.overlay .overlay__title', 2, {
