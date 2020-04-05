ba9c60eb6c16c8eb4be539ea4d861b52b5e104a7
Razvp <razvanpetru@users.noreply.github.com>
2020-04-05 19:06:20 +0300

    .

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/.well-known/acme-challenge/GI7tcccyjZFEK2HtXqcqN1BR_sxPOLXplm_zyAy-cf4', function (req, res) {
+  res.send('GI7tcccyjZFEK2HtXqcqN1BR_sxPOLXplm_zyAy-cf4.ket2r9uGNomFP5YxPui73gw3GW3DMvsBgCRiPnx8Q-s')
+})
+
 app.use((req, res) => {
   res.status(404).sendFile(__dirname + '/views/404.html');
 });
